17b9476ae8a9e6560ca5b6bfe4f10bcd38863557
picoriley <acloudy@mail.smu.edu>
2014-06-28 16:28:01 -0700

    Bricks now spawn and spawn balls upon breaking

--- Unity/Assets/Scripts/Brick.cs
+++ Unity/Assets/Scripts/Brick.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Brick : MonoBehaviour {
 
+	public Transform ball;
 	// Use this for initialization
 	void Start () {
 	
@@ -11,6 +12,10 @@ public class Brick : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void OnDestroy() {
+		Instantiate(ball, new Vector3(this.transform.position.x, this.transform.position.y, 0), Quaternion.identity);
 	}
 
 }
---
+++ Unity/Assets/Spawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public Transform brick;
+	// Use this for initialization
+	void Start () {
+		Spawn ();
+		//InvokeRepeating("Spawn", 5f, 5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Spawn() {
+		for (int y = 0; y < 25; y+=2) {
+			for (int x = -10; x < 25; x+=2) {
+				Instantiate(brick, new Vector3(x, y, 0), Quaternion.identity);
+			}
+		}
+	}
+}
