75230e6256a42abd4abc792508a720e80b6386c6
picoriley <acloudy@mail.smu.edu>
2014-06-28 17:40:27 -0700

    Added a rock <3

---
+++ Unity/Assets/Scripts/Spawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+	public Transform brick;
+
+	public int [][] Level1 = 
+	{
+		new int[] {0,0,1,1,1,1,0,0},
+		new int[] {0,0,1,1,1,1,0,0},
+		new int[] {0,0,1,1,1,1,0,0},
+		new int[] {0,0,1,1,1,1,0,0},
+	};
+	// Use this for initialization
+	void Start () {
+		Spawn ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Spawn() 
+	{
+		float y = 0f;
+		foreach (var layer in Level1) 
+		{
+			float x = -10f;
+			foreach (int position in layer) 
+			{
+				if(position == 1)
+				{
+					Instantiate(brick, new Vector3(x, y, 0f), Quaternion.identity);
+				}
+				x += 3f;
+			}
+			y += 0.8f;
+		}
+	}
+}
