ebe458f2b6959dc42fcbdfc2b8e3d2ce1b4f25ee
picoriley <acloudy@mail.smu.edu>
2014-06-28 20:16:35 -0700

    Added a hidden "removal wall"

--- Unity/Assets/Scripts/OutOfBounds.cs
+++ Unity/Assets/Scripts/OutOfBounds.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class OutOfBounds : MonoBehaviour {
 
 	private int m_numberBallsDestroyed;
 
