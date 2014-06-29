917446c1f10c28a5959db0abddaa844a28b9382a
Drew Jackson <aajackson@smu.edu>
2014-06-29 13:02:22 -0700

    More merging! Paddles and launching balls.

--- Unity/Assets/Scripts/Ball.cs
+++ Unity/Assets/Scripts/Ball.cs
@@ -3,47 +3,82 @@ using System.Collections;
 
 public class Ball : MonoBehaviour
 {
+	public GameObject paddle;
+	/// <summary>
+	/// Check this if this is the first ball in the level
+	/// </summary>
+	public bool initialBall = false;
 
-		private Rigidbody2D m_rb;
-		private bool justSpawned;
+	private Rigidbody2D m_rb;
+	private bool justSpawned;
+	private bool launched = false;
 
-		// Use this for initialization
-		void Start ()
+	// Use this for initialization
+	void Start ()
+	{
+		float speed = 5.0f;
+		justSpawned = true;
+		m_rb = GetComponent< Rigidbody2D > ();
+		if (m_rb)
+		{
+			if (!initialBall) {
+					float randomAngle = Random.Range (0.0f, 360.0f);
+					m_rb.velocity = new Vector2 (speed * Mathf.Cos (Mathf.Deg2Rad * randomAngle), speed * Mathf.Sin (Mathf.Deg2Rad * randomAngle));
+			}
+			else
+			{
+				m_rb.velocity = Vector3.zero;
+				m_rb.angularVelocity = 0.0f;
+			}
+		}
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		//transform.position += m_velocity * Time.deltaTime;
+		if ( !launched && paddle != null ) 
+		{
+			Vector3 newPosition = new Vector3( paddle.transform.position.x, transform.position.y, transform.position.z );
+			transform.position = newPosition;
+			m_rb.velocity = Vector3.zero;
+			m_rb.angularVelocity = 0.0f;
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		//If collided with a brick, destroy it
+		if (collision.gameObject.CompareTag( "Brick"))
+		{
+			if(!justSpawned)
+				Destroy(collision.gameObject);
+		}
+		
+		//If collided withe the paddle, change the angle of the ball based on the direction
+		if (collision.gameObject.name == "Paddle")
+		{
+			if (justSpawned)
+				justSpawned = false;
+		    Vector3 directionToBall = transform.position - collision.gameObject.transform.position;
+			float ballSpeed = 5.0f;
+		    
+		    Vector3 newBallVelocity = directionToBall.normalized * ballSpeed;
+		newBallVelocity.x *= 2;
+			m_rb.velocity = newBallVelocity;
+		}
+	}
+
+	//Launches the first ball
+	public void LaunchBall()
+	{
+		if ( !launched )
 		{
 			float speed = 5.0f;
-			justSpawned = true;
-			m_rb = GetComponent< Rigidbody2D > ();
-			if (m_rb) {
-				m_rb.velocity = new Vector2( speed * Mathf.Cos ( Mathf.Deg2Rad * 45.0f ), speed * Mathf.Sin ( Mathf.Deg2Rad * 45.0f ) );
-			}
+			m_rb.velocity = new Vector2( speed * Mathf.Cos ( Mathf.Deg2Rad * 45.0f ), speed * Mathf.Sin ( Mathf.Deg2Rad * 45.0f ) );
+			launched = true;
 		}
+	}
 
-		// Update is called once per frame
-		void Update ()
-		{
-			//transform.position += m_velocity * Time.deltaTime;
-		}
-
-		void OnCollisionEnter2D (Collision2D collision)
-		{
-			//If collided with a brick, destroy it
-			if (collision.gameObject.CompareTag( "Brick"))
-			{
-				if(!justSpawned)
-					Destroy(collision.gameObject);
-			}
-			
-			//If collided withe the paddle, change the angle of the ball based on the direction
-			if (collision.gameObject.name == "Paddle")
-			{
-				if (justSpawned)
-					justSpawned = false;
-			    Vector3 directionToBall = transform.position - collision.gameObject.transform.position;
-				float ballSpeed = 5.0f;
-			    
-			    Vector3 newBallVelocity = directionToBall.normalized * ballSpeed;
-     			newBallVelocity.x *= 2;
-				m_rb.velocity = newBallVelocity;
-			}
-		}
 }
--- Unity/Assets/Scripts/Controller.cs
+++ Unity/Assets/Scripts/Controller.cs
@@ -6,6 +6,8 @@ public class Controller : MonoBehaviour {
 	public float moveForce = 365f;			// Amount of force added to move the player left and right.
 	public float maxSpeed = 20f;				// The fastest the player can travel in the x axis.
 	public float currMaxSpeed = 20f;
+
+
 	// Use this for initialization
 	void Start () {
 		currMaxSpeed = maxSpeed;
@@ -18,6 +20,8 @@ public class Controller : MonoBehaviour {
 		//Grab the horizontal movement from wasd or arrow keys.
 		//GetAxisRaw lets the paddle stop on a dime once the button is let up.
 		float horizontalInput = Input.GetAxisRaw ("Horizontal");
+	    
+
 		//If you hold down shift, you can move faster
 		if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.LeftShift))
 			currMaxSpeed = maxSpeed * 2f;
@@ -25,5 +29,20 @@ public class Controller : MonoBehaviour {
 			currMaxSpeed = maxSpeed;
 		
 		this.rigidbody2D.velocity = Vector2.right * horizontalInput * currMaxSpeed;
+
+		//Launch the ball when spacebar is pressed
+		if( Input.GetKeyDown( KeyCode.Space ) )
+		{
+			var balls = GameObject.FindGameObjectsWithTag( "Ball" );
+
+			foreach( GameObject gameobject in balls )
+			{
+				Ball ball = (Ball) gameobject.GetComponent( typeof( Ball ) );
+				if( ball.initialBall )
+				{
+					ball.LaunchBall();
+				}
+			}
+		}
 	}
 }
