5abb5b9300cc3f80c59cb69bbdaaaf1b18c8f8f2
Drew Jackson <aajackson@smu.edu>
2014-06-29 20:10:52 -0700

    FINAL COMMITS!

---
+++ Unity/Assets/Scripts/CloseOnClick.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloseOnClick : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	
+	void OnMouseDown()
+	{
+		Spawner theSpawner = FindObjectOfType (typeof(Spawner)) as Spawner;
+		//theSpawner.nextLevel ();
+		Destroy ( this );
+	}
+}
